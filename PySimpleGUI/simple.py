b1438040e51703be884881cb56ae98b6a35b16bf
massao000 <onepiece.4224y@icloud.com>
2021-03-02 18:13:45 +0900

    0302

--- PySimpleGUI/simple.py
+++ PySimpleGUI/simple.py
@@ -5,24 +5,6 @@ sg.theme("dark blue")
 
 number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# tab1_layout = [
-#         [sg.Text('▼表示テキストの入力▼')],
-#         [sg.Input(key='in-text')],
-#         [sg.Text('ここの文字が変わります', size=(50,1), key='out-text')],
-#         [sg.Checkbox('チェックボックス->', key='test', change_submits=True), sg.Text('off', size=(3,1), key='Check')],
-#         [sg.Button('実行', button_color=('midnightblue', '#87cefa')), 
-#         sg.Button('pop', button_color=('midnightblue', '#87cefa'))],
-#         ] 
-
-# tab2_layout =[
-#         [sg.Text('ファルダ選択', size=(13, 1)), sg.Input(size=(20, 1)), 
-#         sg.FileBrowse('ファイル選択', key='file', button_color=('midnightblue', '#87cefa'))],
-#         [sg.Button('表示', key='hyouji', button_color=('midnightblue', '#87cefa'))],
-#         [sg.Combo((number_list), default_value='ナンバーズ', size=(10, 1)),
-#         sg.Listbox(number_list, size=(10, 5)), 
-#         sg.Slider(range=(0.0,100.0), default_value =0.0, resolution=1.0, orientation='h', size=(35, None))]
-#         ]
-
 tab3_layout = [[sg.Text('タブ1', size=(10,3))]]
 tab4_layout = [[sg.Text('タブ2', size=(10,3))]]
 layout = [
@@ -30,9 +12,8 @@ layout = [
     [sg.Input(key='in-text'), sg.Button('実行', button_color=('midnightblue', '#87cefa'))],
     [sg.Text('ここの文字が変わります', size=(50,1), key='out-text')],
     [sg.Checkbox('チェックボックス->', key='test', change_submits=True), sg.Text('off', size=(3,1), key='Check')],
-    [ sg.Button('pop', button_color=('midnightblue', '#87cefa'))],
-    [sg.Text('ファルダ選択', size=(13, 1)),
-        sg.Input(size=(20, 1)), 
+    [sg.Button('pop', button_color=('midnightblue', '#87cefa'))],
+    [sg.Text('ファルダ選択', size=(13, 1)),sg.Input(size=(20, 1)), 
         sg.FileBrowse('ファイル選択', key='file', button_color=('midnightblue', '#87cefa'))],
 
     [sg.Button('ファイルパス表示', key='hyouji', button_color=('midnightblue', '#87cefa'))],
@@ -42,8 +23,6 @@ layout = [
                                               sg.Tab('T2', tab4_layout)]])
                                               ],
     [sg.Slider(range=(0.0,100.0), default_value =0.0, resolution=1.0, orientation='h', size=(35, None))],
-    
-    []
     ]
 
 window = sg.Window('PySimpleGUI demo', layout)
