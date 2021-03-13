78a69bdc57a1dcc8626688fe56b1240a06b2a82a
massao000 <onepiece.4224y@icloud.com>
2021-03-14 03:06:55 +0900

    0314

--- PySimpleGUI/simple_main.py
+++ PySimpleGUI/simple_main.py
@@ -1,28 +1,28 @@
 import PySimpleGUI as sg
 
-
 sg.theme("dark blue")
 
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-tab1_layout = [
-        [sg.Text('▼表示テキストの入力▼')],
-        [sg.Input(key='in-text')],
-        [sg.Text('ここの文字が変わります', size=(50,1), key='out-text')],
-        [sg.Checkbox('チェックボックス->', key='test'), sg.Text('off', size=(3,1), key='Check')],
-        [sg.Button('実行', button_color=('midnightblue', '#87cefa')), 
-        sg.Button('pop', button_color=('midnightblue', '#87cefa'))],
-        ]
-
-tab2_layout =[
-        [sg.Text('ファルダ選択', size=(15, 1)), sg.Input(size=(20, 1)), 
-        sg.FileBrowse('ファイル選択', button_color=('midnightblue', '#87cefa'))],
-        ]
-
+tab3_layout = [[sg.Text('タブ1', size=(10,3))]]
+tab4_layout = [[sg.Text('タブ2', size=(10,3))]]
 layout = [
-    [sg.TabGroup(
-        [[sg.Tab('T1', tab1_layout), 
-        sg.Tab('T2', tab2_layout)]]
-    )]
+    [sg.Text('▼表示テキストの入力▼')],
+    [sg.Input(key='in-text'), sg.Button('実行', button_color=('midnightblue', '#87cefa'))],
+    [sg.Text('ここの文字が変わります', size=(50,1), key='out-text')],
+    [sg.Checkbox('チェックボックス->', key='test', change_submits=True), sg.Text('off', size=(3,1), key='Check')],
+    [sg.Button('pop', button_color=('midnightblue', '#87cefa'))],
+    [sg.Text('ファルダ選択', size=(13, 1)),sg.Input(size=(20, 1)), 
+        sg.FileBrowse('ファイル選択', key='file', button_color=('midnightblue', '#87cefa'))],
+
+    [sg.Button('ファイルパス表示', key='hyouji', button_color=('midnightblue', '#87cefa'))],
+    [sg.Combo((number_list), default_value='コンボボックス', size=(15, 1)),
+        sg.Listbox(number_list, size=(10, 5)), 
+            sg.TabGroup([
+                [sg.Tab('T1', tab3_layout), 
+                 sg.Tab('T2', tab4_layout)]])
+    ],
+    [sg.Slider(range=(0.0,100.0), default_value =0.0, resolution=1.0, orientation='h', size=(35, None))],
     ]
 
 window = sg.Window('PySimpleGUI demo', layout)
@@ -41,9 +41,17 @@ while True:
     text_convert = ''
     if values['test']:
         text_convert = 'on'
-        # window['Check'].update('on')
     else:
         text_convert = 'off'
     text_elem.update(text_convert)
+
+    if event == 'hyouji':
+        if values["file"] != '':
+            sg.Popup(values['file'])
+        else:
+            sg.Popup('ファイルが選択されていない')
+
     print(event, values)
 window.close()
+
+# 54行
