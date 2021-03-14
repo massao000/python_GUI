f1100e2da6ad433df2cdcab21c312a9a6715bf6f
massao000 <69783019+massao000@users.noreply.github.com>
2021-03-15 01:50:03 +0900

    Add files via upload

--- tkinter/サンプル/button.py
+++ tkinter/サンプル/button.py
@@ -14,18 +14,21 @@ window.title('tkinter demo button')
 frame = ttk.Frame(window)
 frame.grid()
 
+label_check = ttk.Label(frame, text='チェックボックス')
+label_check.grid(row=0, column=1, sticky=tk.W)
+
 bln = tk.BooleanVar()
 # bln.set(False)
 changebox = tk.Checkbutton(frame, variable=bln, text='1')
-changebox.grid(row=0, column=0)
+changebox.grid(row=1, column=0)
 
 bln = tk.BooleanVar()
 changebox = tk.Checkbutton(frame, variable=bln, text='2')
-changebox.grid(row=0, column=1)
+changebox.grid(row=1, column=1)
 
 bln = tk.BooleanVar()
 changebox = tk.Checkbutton(frame, variable=bln, text='3')
-changebox.grid(row=0, column=2)
+changebox.grid(row=1, column=2)
 
 # chk_txt = [1, 2, 3]
 
@@ -36,18 +39,20 @@ changebox.grid(row=0, column=2)
 #     chk = tkinter.Checkbutton(frame, variable=chk_bln[i], text=chk_txt[i]) 
 #     chk.grid(row=0, column=0 + i)
 
+label_check = ttk.Label(frame, text='ラジオボタン')
+label_check.grid(row=2, column=1, sticky=tk.W)
 
 radio = tk.IntVar()
 radio.set(0)
 changebox = tk.Radiobutton(frame, value=0, variable=radio, text='1')
-changebox.grid(row=1, column=0)
+changebox.grid(row=3, column=0)
 changebox = tk.Radiobutton(frame, value=1, variable=radio, text='2')
-changebox.grid(row=1, column=1)
+changebox.grid(row=3, column=1)
 changebox = tk.Radiobutton(frame, value=2, variable=radio, text='3')
-changebox.grid(row=1, column=2)
+changebox.grid(row=3, column=2)
 
 button = ttk.Button(frame, text='ボタン')
-button.grid(row=2, column=1, sticky=tk.W)
+button.grid(row=4, column=1, sticky=tk.W)
 # button.bind('<Button-1>', change_text)
 
 frame.mainloop()
